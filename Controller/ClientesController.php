f03117dfc1fb67f33214fc2733fbc5446bd23ffa
Cristian Rojas <cristian18@outlook.cl>
2017-05-24 16:48:32 -0400

    Agregado exportar clientes

--- Controller/ClientesController.php
+++ Controller/ClientesController.php
@@ -169,5 +169,25 @@ class ClientesController extends AppController {
     	echo json_encode($cliente);
     	exit;
     }
+
+    public function admin_exportar()
+
+    {
+
+        $datos          = $this->Cliente->find('all', array(
+
+            'recursive'             => -1
+
+        ));
+
+        $campos         = array_keys($this->Cliente->_schema);
+
+        $modelo         = $this->Cliente->alias;
+
+
+
+        $this->set(compact('datos', 'campos', 'modelo'));
+
+    }
     
 }
