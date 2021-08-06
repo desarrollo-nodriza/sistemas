c5287ed71b85c35b310af9b72a3c3850ca95a9f5
Cuerdo94Nodriza <diego.romero@nodriza.cl>
2021-08-06 14:00:14 -0400

    Sistemas - Validar oc manual

--- Controller/OrdenComprasController.php
+++ Controller/OrdenComprasController.php
@@ -1988,13 +1988,16 @@ class OrdenComprasController extends AppController
 			$qry = array(
 				'conditions' => array(
 					'OrdenCompra.id' => $id,
-					'OrdenCompra.estado' => array('recepcion_incompleta', 'espera_recepcion')
+					'OrdenCompra.estado' => array('recepcion_incompleta', 'espera_recepcion'),
 				),
 				'contain' => array(
 					'VentaDetalleProducto' => array(
 						'fields' => array(
 							'VentaDetalleProducto.id'
-						)
+						),
+						'conditions'=>[
+							'OrdenComprasVentaDetalleProducto.cantidad_recibida != OrdenComprasVentaDetalleProducto.cantidad_validada_proveedor' 
+						]
 					)
 				), 
 				'fields' => array(
@@ -2003,6 +2006,8 @@ class OrdenComprasController extends AppController
 			);
 
 			$this->request->data = $this->OrdenCompra->find('first', $qry);
+
+			
 		}
 
 		if ( empty($this->request->data) )
