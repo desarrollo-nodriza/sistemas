1f54720301769577337d59f09e58c5da3a36aed7
Diego Romero <diego.romero@nodriza.cl>
2023-01-27 22:14:34 -0300

    json disponibilidad full

--- Console/Command/ActualizarJsonDisponibilidadShell.php
+++ Console/Command/ActualizarJsonDisponibilidadShell.php
@@ -1,15 +1,17 @@
-<?php 
+<?php
 
 App::uses('CakeRequest', 'Network');
 App::uses('CakeResponse', 'Network');
 App::uses('Controller', 'Controller');
 App::uses('VentaDetalleProductosController', 'Controller');
 
-class ActualizarJsonDisponibilidadShell extends AppShell {
+class ActualizarJsonDisponibilidadShell extends AppShell
+{
 
-	public function main() {
+	public function main()
+	{
 
-        $log = array();
+		$log = array();
 
 		$log[] = array('Log' => array(
 			'administrador' => 'Demonio',
@@ -17,19 +19,20 @@ class ActualizarJsonDisponibilidadShell extends AppShell {
 			'modulo_accion' => 'Inicia proceso de actualización de json de disponibilidad: ' . date('Y-m-d H:i:s')
 		));
 
-        $controller = new VentaDetalleProductosController(new CakeRequest(), new CakeResponse());
+		$controller = new VentaDetalleProductosController(new CakeRequest(), new CakeResponse());
 
-        # Actualizamos la base de datos con los cambios en los envios
-        $resultado = $controller->generar_json_productos_disponibles();
+		# Actualizamos la base de datos con los cambios en los envios
+		$resultado[] = $controller->generar_json_productos_disponibles();
+		$resultado[] = $controller->admin_disponibilidad_full();
 
-        $log[] = array('Log' => array(
+		$log[] = array('Log' => array(
 			'administrador' => 'Demonio',
 			'modulo' => 'Ventas',
 			'modulo_accion' => 'Finaliza proceso de actualización de json de disponibilidad: Actualizado - ' . json_encode($resultado)
 		));
-    
-        # Guardamos el log
-        ClassRegistry::init('Log')->saveMany($log);
+
+		# Guardamos el log
+		ClassRegistry::init('Log')->saveMany($log);
 
 		return true;
 	}
